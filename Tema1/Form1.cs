86b0654bab02a1cf5701f61a49da18fe2140dfd9
mcdobr <mircea.dobreanu@gmail.com>
2018-02-17 13:35:18 +0200

    Hopeful development done for first hw. Need to add edge cases and tests. Also need to add unit tests.

--- Tema1/Form1.cs
+++ Tema1/Form1.cs
@@ -12,6 +12,27 @@ namespace Tema1
 {
     public partial class CalculatorForm : Form
     {
+        private static char GUARDIAN_OPERATION = '#';
+        private static Dictionary<char, int> operatorPriorityInExpression = new Dictionary<char, int>()
+        {
+            {'#', -1},
+            {'+', 1}, {'-', 1},
+            {'*', 2}, {'/', 2},
+            {'^', 4},
+            {'(', 5},
+        };
+        private static Dictionary<char, int> operatorPriorityInStack = new Dictionary<char, int>()
+        {
+            {'#', -1},
+            {'+', 1}, {'-', 1},
+            {'*', 2}, {'/', 2},
+            {'^', 3},
+            {'(', 0},
+        };
+
+        private static char[] operators = {'+', '-', '*', '/', '^', '('};
+
+
         public CalculatorForm()
         {
             InitializeComponent();
@@ -33,23 +54,103 @@ namespace Tema1
                 calcBox.Text = calcBox.Text.Remove(calcBox.Text.Length - 1);
         }
 
+        private bool isOperator(char c)
+        {
+            return operators.Contains(c);
+        }
 
         private string postfix_representation(string expr)
         {
             StringBuilder sb = new StringBuilder();
 
-            Stack<string> operatorStack, operandStack;
+            Stack<char> operatorStack = new Stack<char>();
+            operatorStack.Push(GUARDIAN_OPERATION);
+            
+            foreach (char c in expr)
+            {
+                if (Char.IsDigit(c) || c == '.')
+                {
+                    sb.Append(c);
+                }
+                else if (isOperator(c))
+                {
+                    sb.Append(' ');
+                    if (operatorPriorityInExpression[c] <= operatorPriorityInStack[operatorStack.Peek()])
+                        sb.Append(operatorStack.Pop());
+                    operatorStack.Push(c);
+                }
+                else if (c == ')')
+                {
+                    sb.Append(' ');
+                    while (operatorStack.Peek() != '(')
+                        sb.Append(operatorStack.Pop());
+                    operatorStack.Pop();
+                }
+            }
 
-
-
-
-            return "";
+            while (operatorStack.Peek() != GUARDIAN_OPERATION)
+            {
+                sb.Append(' ');
+                sb.Append(operatorStack.Pop());
+            }
+            return sb.ToString().Trim();
         }
 
+        private double eval(string postfix_repr)
+        {
+            double result = 0.0;
+
+            string[] tokens = postfix_repr.Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+            Stack<double> operandStack = new Stack<double>();
+
+
+            foreach (string token in tokens)
+            {
+                if (isOperator(token[0]))
+                {
+                    double operandRight = operandStack.Pop();
+                    double operandLeft = operandStack.Pop();
+
+                    double intermediary_res = 0;
+                    switch (token[0])
+                    {
+                        case '+':
+                            intermediary_res = operandLeft + operandRight;
+                            break;
+                        case '-':
+                            intermediary_res = operandLeft - operandRight;
+                            break;
+                        case '*':
+                            intermediary_res = operandLeft * operandRight;
+                            break;
+                        case '/':
+                            intermediary_res = operandLeft / operandRight;
+                            break;
+                        case '^':
+                            intermediary_res = Math.Pow(operandLeft, operandRight);
+                            break;
+                    }
+                    operandStack.Push(intermediary_res);
+
+                }
+                else
+                {
+                    operandStack.Push(Double.Parse(token));
+                }
+            }
+
+            result = operandStack.Pop();
+            return result;
+        }
 
         private void buttonEquals_click(object sender, EventArgs e)
         {
+            string postfix_expr = postfix_representation(this.calcBox.Text);
+            double result = eval(postfix_expr);
+
+            this.calcBox.Text = result.ToString();
             
+            this.textBox1.Text = postfix_expr;
         }
     }
 }
