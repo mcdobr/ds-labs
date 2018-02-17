9c462b0376fc344d14150d4e060e5cee9ab749d4
mcdobr <mircea.dobreanu@gmail.com>
2018-02-17 09:42:32 +0200

    Added in-lab work.

--- Lab1/Form1.Designer.cs
+++ Lab1/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Lab1
+﻿namespace WindowsFormsApplication1
 {
     partial class Form1
     {
@@ -28,12 +28,147 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonMinus = new System.Windows.Forms.Button();
+            this.buttonPlus = new System.Windows.Forms.Button();
+            this.buttonMultiplication = new System.Windows.Forms.Button();
+            this.buttonDivision = new System.Windows.Forms.Button();
+            this.buttonEquals = new System.Windows.Forms.Button();
+            this.buttonPower = new System.Windows.Forms.Button();
+            this.operand1 = new System.Windows.Forms.TextBox();
+            this.operand2 = new System.Windows.Forms.TextBox();
+            this.result = new System.Windows.Forms.TextBox();
+            this.buttonMod = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonMinus
+            // 
+            this.buttonMinus.Location = new System.Drawing.Point(185, 173);
+            this.buttonMinus.Name = "buttonMinus";
+            this.buttonMinus.Size = new System.Drawing.Size(75, 23);
+            this.buttonMinus.TabIndex = 0;
+            this.buttonMinus.Text = "-";
+            this.buttonMinus.UseVisualStyleBackColor = true;
+            this.buttonMinus.Click += new System.EventHandler(this.button_click);
+            // 
+            // buttonPlus
+            // 
+            this.buttonPlus.Location = new System.Drawing.Point(104, 173);
+            this.buttonPlus.Name = "buttonPlus";
+            this.buttonPlus.Size = new System.Drawing.Size(75, 23);
+            this.buttonPlus.TabIndex = 1;
+            this.buttonPlus.Text = "+";
+            this.buttonPlus.UseVisualStyleBackColor = true;
+            this.buttonPlus.Click += new System.EventHandler(this.button_click);
+            // 
+            // buttonMultiplication
+            // 
+            this.buttonMultiplication.Location = new System.Drawing.Point(104, 144);
+            this.buttonMultiplication.Name = "buttonMultiplication";
+            this.buttonMultiplication.Size = new System.Drawing.Size(75, 23);
+            this.buttonMultiplication.TabIndex = 2;
+            this.buttonMultiplication.Text = "*";
+            this.buttonMultiplication.UseVisualStyleBackColor = true;
+            this.buttonMultiplication.Click += new System.EventHandler(this.button_click);
+            // 
+            // buttonDivision
+            // 
+            this.buttonDivision.Location = new System.Drawing.Point(185, 144);
+            this.buttonDivision.Name = "buttonDivision";
+            this.buttonDivision.Size = new System.Drawing.Size(75, 23);
+            this.buttonDivision.TabIndex = 3;
+            this.buttonDivision.Text = "/";
+            this.buttonDivision.UseVisualStyleBackColor = true;
+            this.buttonDivision.Click += new System.EventHandler(this.button_click);
+            // 
+            // buttonEquals
+            // 
+            this.buttonEquals.Location = new System.Drawing.Point(185, 202);
+            this.buttonEquals.Name = "buttonEquals";
+            this.buttonEquals.Size = new System.Drawing.Size(75, 23);
+            this.buttonEquals.TabIndex = 4;
+            this.buttonEquals.Text = "=";
+            this.buttonEquals.UseVisualStyleBackColor = true;
+            this.buttonEquals.Click += new System.EventHandler(this.button_click);
+            // 
+            // buttonPower
+            // 
+            this.buttonPower.Location = new System.Drawing.Point(104, 115);
+            this.buttonPower.Name = "buttonPower";
+            this.buttonPower.Size = new System.Drawing.Size(75, 23);
+            this.buttonPower.TabIndex = 5;
+            this.buttonPower.Text = "^";
+            this.buttonPower.UseVisualStyleBackColor = true;
+            this.buttonPower.Click += new System.EventHandler(this.button_click);
+            // 
+            // operand1
+            // 
+            this.operand1.Location = new System.Drawing.Point(12, 12);
+            this.operand1.Name = "operand1";
+            this.operand1.Size = new System.Drawing.Size(100, 20);
+            this.operand1.TabIndex = 6;
+            // 
+            // operand2
+            // 
+            this.operand2.Location = new System.Drawing.Point(12, 38);
+            this.operand2.Name = "operand2";
+            this.operand2.Size = new System.Drawing.Size(100, 20);
+            this.operand2.TabIndex = 7;
+            // 
+            // result
+            // 
+            this.result.Location = new System.Drawing.Point(12, 64);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(100, 20);
+            this.result.TabIndex = 8;
+            // 
+            // buttonMod
+            // 
+            this.buttonMod.Location = new System.Drawing.Point(185, 115);
+            this.buttonMod.Name = "buttonMod";
+            this.buttonMod.Size = new System.Drawing.Size(75, 23);
+            this.buttonMod.TabIndex = 10;
+            this.buttonMod.Text = "%";
+            this.buttonMod.UseVisualStyleBackColor = true;
+            this.buttonMod.Click += new System.EventHandler(this.button_click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.buttonMod);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.operand2);
+            this.Controls.Add(this.operand1);
+            this.Controls.Add(this.buttonPower);
+            this.Controls.Add(this.buttonEquals);
+            this.Controls.Add(this.buttonDivision);
+            this.Controls.Add(this.buttonMultiplication);
+            this.Controls.Add(this.buttonPlus);
+            this.Controls.Add(this.buttonMinus);
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonMinus;
+        private System.Windows.Forms.Button buttonPlus;
+        private System.Windows.Forms.Button buttonMultiplication;
+        private System.Windows.Forms.Button buttonDivision;
+        private System.Windows.Forms.Button buttonEquals;
+        private System.Windows.Forms.Button buttonPower;
+        private System.Windows.Forms.Button buttonMod;
+        private System.Windows.Forms.TextBox operand1;
+        private System.Windows.Forms.TextBox operand2;
+        private System.Windows.Forms.TextBox result;
+
+
+        // Mine
+        private string operation;
     }
 }
 
--- Lab1/Form1.cs
+++ Lab1/Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Lab1
+namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
@@ -16,5 +16,60 @@ namespace Lab1
         {
             InitializeComponent();
         }
+
+
+        private void button_click(object sender, EventArgs e)
+        {
+            string operation = ((Button)sender).Text;
+            bool divisionByZero = false;
+
+            if (operation == "=")
+            {
+
+                double operand1 = Double.Parse(this.operand1.Text);
+                double operand2 = Double.Parse(this.operand2.Text);
+                double res = 0.0;
+
+                switch (this.operation)
+                {
+                    case "+":
+                        res = operand1 + operand2;
+                        break;
+                    case "-":
+                        res = operand1 - operand2;
+                        break;
+                    case "*":
+                        res = operand1 * operand2;
+                        break;
+                    case "/":
+                        if (Math.Abs(operand2) < 1e-6)
+                        {
+                            divisionByZero = true;
+                        }
+                        res = operand1 / operand2;
+                        break;
+                    case "^":
+                        res = Math.Pow(operand1, operand2);
+                        break;
+                    case "%":
+                        res = operand1 % operand2;
+                        if (res < 0)
+                            res += operand2;
+                        break;
+                }
+                if (divisionByZero)
+                {
+                    this.result.Text = "Division by zero";
+                    return;
+                }
+                this.result.Text = res.ToString();
+            }
+            else
+            {
+                this.operation = operation;
+            }
+            
+        }
+        
     }
 }
