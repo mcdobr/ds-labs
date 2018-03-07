c2fed93ec1a84439bc6899bd716a6839c625668d
mcdobr <mircea.dobreanu@gmail.com>
2018-03-07 19:42:03 +0200

    Added lab4.

---
+++ ClientRMI/ClientMain.cs
@@ -0,0 +1,26 @@
+﻿using MathLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathClient
+{
+    class ClientMain
+    {
+        static void Main(string[] args)
+        {
+            RemotingConfiguration.RegisterActivatedClientType(typeof(MathLibrary.Customer),
+          "http://localhost:10000");
+            // Calling a nondefault constructor. No exceptions now because
+            // Customer is a client-activated object.
+            Customer cust = new Customer("Homer");
+            Console.WriteLine(cust.SayHello());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MathLibrary/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLibrary
+{
+    public class Customer : MarshalByRefObject
+    {
+        string mName;
+        public Customer(string name)
+        {
+            Console.WriteLine("Customer.ctor({0})", name);
+            mName = name;
+        }
+        public string SayHello()
+        {
+            Console.WriteLine("Customer.SayHello()");
+            return "Hello " + mName;
+        }
+    }
+}
