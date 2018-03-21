bc6b80f41869c0af0932342ef1d74afed2dd4595
mcdobr <mircea.dobreanu@gmail.com>
2018-03-21 19:42:17 +0200

    blank non-working lab6.

---
+++ Lab6/TemaLab/WebApp/EmployeeService.asmx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Xml.Serialization;
+
+namespace WebApp
+{
+    /// <summary>
+    /// Summary description for MyWebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class EmployeeService : WebService
+    {
+        [WebMethod]
+        public int add(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+
+        [WebMethod]
+        EmployeeData getEmployee(int id)
+        {
+            return new EmployeeData(id, "homer", "333-33-3333");
+        }
+    }
+
+    public class EmployeeData
+    {
+        string name, ssn;
+        [XmlAttribute()]
+        int id;
+
+        public EmployeeData() { }
+
+        public EmployeeData(int id, string name, string ssn)
+        {
+            this.id = id;
+            this.name = name;
+            this.ssn = ssn;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("ID={0};Name={1};SSN={2}", id, name, ssn);
+        }
+    }
+}
---
+++ Lab6/TemaLab/WebClient/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace WebClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeService myWebService = new EmployeeService();
+            Console.WriteLine("2 + 3 = {0}", myWebService.add(2, 3));
+
+            myWebService.getEmployee(2);
+        }
+    }
+}
