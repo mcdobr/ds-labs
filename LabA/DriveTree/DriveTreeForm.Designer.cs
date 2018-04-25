742318bbc0a8d80a0468f3faabc54f3264d83350
mcdobr <mircea.dobreanu@gmail.com>
2018-04-25 19:03:00 +0300

    Added lab10 hw.

---
+++ LabA/DriveTree/DriveTreeForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace DriveTree
+{
+    partial class DriveTreeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.browseButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // browseButton
+            // 
+            this.browseButton.Location = new System.Drawing.Point(197, 226);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(75, 23);
+            this.browseButton.TabIndex = 0;
+            this.browseButton.Text = "Browse";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // DriveTreeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.browseButton);
+            this.Name = "DriveTreeForm";
+            this.Text = "DriveTree";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button browseButton;
+    }
+}
+
---
+++ LabA/DriveTree/DriveTreeForm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace DriveTree
+{
+    public partial class DriveTreeForm : Form
+    {
+        private string dir;
+        public DriveTreeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog selectedDir = new FolderBrowserDialog();
+            if (selectedDir.ShowDialog() == DialogResult.OK)
+            {
+                dir = selectedDir.SelectedPath;
+                DirectoryInfo dirInfo = new DirectoryInfo(dir);
+
+
+                XElement xmlInfo = GetDirXML(dirInfo);
+                File.WriteAllText(dirInfo.Name + "Listing.xml", xmlInfo.ToString());
+            }
+        
+        }
+
+        private static XElement GetDirXML(DirectoryInfo dir)
+        {
+            XElement xmlInfo = new XElement("dir",
+                new XAttribute("name", dir.Name));
+
+            foreach (var file in dir.GetFiles())
+            {
+                xmlInfo.Add(new XElement("file",
+                    new XAttribute("name", file.Name)));
+            }
+
+            // Recursively call on the subdirs
+            foreach (var subDir in dir.GetDirectories())
+            {
+                xmlInfo.Add(GetDirXML(subDir));
+            }
+            return xmlInfo;
+        }
+
+    }
+}
