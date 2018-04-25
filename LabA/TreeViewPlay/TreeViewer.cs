1553e5a84985916ba0d059803e4e702d50737c7f
mcdobr <mircea.dobreanu@gmail.com>
2018-04-25 19:34:50 +0300

    Added blank lw.

---
+++ LabA/TreeViewPlay/TreeViewer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace TreeViewPlay
+{
+    public partial class TreeViewer : Form
+    {
+        public TreeViewer()
+        {
+            InitializeComponent();
+        }
+
+        private void TreeViewer_Load(object sender, EventArgs e)
+        {
+            TreeNode root;
+            XmlDocument xmlDoc = new XmlDocument();
+        }
+    }
+}
