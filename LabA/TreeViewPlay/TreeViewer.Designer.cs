30762439344c4a69b1a3c24410244fc59ecfe6bc
mcdobr <mircea.dobreanu@gmail.com>
2018-04-28 21:00:48 +0300

    Can now parse XML into treeview.

--- LabA/TreeViewPlay/TreeViewer.Designer.cs
+++ LabA/TreeViewPlay/TreeViewer.Designer.cs
@@ -28,32 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.openButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // treeView1
+            // treeView
             // 
-            this.treeView1.Location = new System.Drawing.Point(12, 12);
-            this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(375, 307);
-            this.treeView1.TabIndex = 0;
+            this.treeView.Location = new System.Drawing.Point(12, 12);
+            this.treeView.Name = "treeView";
+            this.treeView.Size = new System.Drawing.Size(572, 387);
+            this.treeView.TabIndex = 0;
+            this.treeView.AfterCheck += new System.Windows.Forms.TreeViewEventHandler(this.treeView_AfterCheck);
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(428, 405);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(75, 23);
+            this.openButton.TabIndex = 1;
+            this.openButton.Text = "Open XML";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(509, 405);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 2;
+            this.saveButton.Text = "saveButton";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
             // 
             // TreeViewer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(596, 440);
-            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.openButton);
+            this.Controls.Add(this.treeView);
             this.Name = "TreeViewer";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.TreeViewer_Load);
+            this.Text = "TreeViewer";
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TreeView treeView;
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.Button saveButton;
     }
 }
 
--- LabA/TreeViewPlay/TreeViewer.cs
+++ LabA/TreeViewPlay/TreeViewer.cs
@@ -3,25 +3,99 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml;
+using System.Xml.Linq;
 
 namespace TreeViewPlay
 {
     public partial class TreeViewer : Form
     {
+        private static readonly string ROOT_PROJECT_FOLDER = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+
         public TreeViewer()
         {
             InitializeComponent();
         }
-
-        private void TreeViewer_Load(object sender, EventArgs e)
+        
+        private void openButton_Click(object sender, EventArgs e)
         {
-            TreeNode root;
-            XmlDocument xmlDoc = new XmlDocument();
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.InitialDirectory = ROOT_PROJECT_FOLDER;
+            openDialog.Filter = "XML files|*.xml";
+            openDialog.FilterIndex = 0;
+            openDialog.DefaultExt = "xml";
+
+
+            if (openDialog.ShowDialog() == DialogResult.OK)
+            {
+                if (!string.Equals(Path.GetExtension(openDialog.FileName), ".xml",
+                    StringComparison.OrdinalIgnoreCase))
+                {
+                    MessageBox.Show("", "",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    /* User has really selected an XML */
+                    XDocument xDoc = XDocument.Load(openDialog.FileName);
+                    XElement xRoot = xDoc.Root;
+
+                    treeView.CheckBoxes = true;
+                    treeView.Nodes.Clear();
+                    TreeNode tRoot = new TreeNode(stringifyXTag(xRoot));
+
+                    treeView.Nodes.Add(tRoot);
+                    addTreeNode(xRoot, tRoot);
+                }
+            }
+
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveDialog = new SaveFileDialog();
+
+            if (saveDialog.ShowDialog() == DialogResult.OK)
+            {
+                XDocument xDoc = new XDocument();
+            }
+        }
+
+        private void addTreeNode(XElement xElement, TreeNode tElement)
+        {
+            foreach (var xChild in xElement.Elements())
+            {
+                TreeNode tChild = new TreeNode(stringifyXTag(xChild));
+                tElement.Nodes.Add(tChild);
+
+                addTreeNode(xChild, tChild); 
+            }
+        }
+
+        private string stringifyXTag(XElement xElement)
+        {
+            if (xElement.HasElements)
+                return xElement.Name + " " + string.Join(" ", xElement.Attributes());
+            else
+                return xElement.Name + " " + string.Join(" ", xElement.Attributes()) + xElement.Value;
+
+        }
+
+        private void treeView_AfterCheck(object sender, TreeViewEventArgs e)
+        {
+            TreeNode root = e.Node;
+            if (root.Nodes.Count > 0)
+            {
+                foreach (TreeNode child in root.Nodes)
+                {
+                    child.Checked = root.Checked;
+                }
+            }
         }
     }
 }
