47820cd54af866d2c0ed78b1086b5d722f4b2428
mcdobr <mircea.dobreanu@gmail.com>
2018-05-02 19:39:03 +0300

    Added full labwork for lab11.

---
+++ LabB/Frenglish/FrenglishForm.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Frenglish
+{
+    partial class FrenglishForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.hiButton = new System.Windows.Forms.Button();
+            this.languageCombobox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // nameBox
+            // 
+            this.nameBox.Location = new System.Drawing.Point(427, 252);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(100, 20);
+            this.nameBox.TabIndex = 0;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(371, 255);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(35, 13);
+            this.nameLabel.TabIndex = 1;
+            this.nameLabel.Text = "Name";
+            // 
+            // hiButton
+            // 
+            this.hiButton.Location = new System.Drawing.Point(557, 250);
+            this.hiButton.Name = "hiButton";
+            this.hiButton.Size = new System.Drawing.Size(75, 23);
+            this.hiButton.TabIndex = 2;
+            this.hiButton.Text = "Hi";
+            this.hiButton.UseVisualStyleBackColor = true;
+            this.hiButton.Click += new System.EventHandler(this.hiButton_Click);
+            // 
+            // languageCombobox
+            // 
+            this.languageCombobox.FormattingEnabled = true;
+            this.languageCombobox.Items.AddRange(new object[] {
+            "en-GB",
+            "fr-FR"});
+            this.languageCombobox.Location = new System.Drawing.Point(121, 149);
+            this.languageCombobox.Name = "languageCombobox";
+            this.languageCombobox.Size = new System.Drawing.Size(121, 21);
+            this.languageCombobox.Sorted = true;
+            this.languageCombobox.TabIndex = 3;
+            this.languageCombobox.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // FrenglishForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(688, 436);
+            this.Controls.Add(this.languageCombobox);
+            this.Controls.Add(this.hiButton);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.nameBox);
+            this.Name = "FrenglishForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Button hiButton;
+        private System.Windows.Forms.ComboBox languageCombobox;
+    }
+}
+
---
+++ LabB/Frenglish/FrenglishForm.cs
@@ -0,0 +1,38 @@
+﻿using Frenglish.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Frenglish
+{
+    public partial class FrenglishForm : Form
+    {
+        private ResourceManager resManager = new ResourceManager("Languages.Strings", System.Reflection.Assembly.LoadFrom("Languages.dll"));
+        public FrenglishForm()
+        {
+            InitializeComponent();
+        }
+
+        private void hiButton_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show(CultureInfo.CurrentUICulture.ToString());
+            string name = nameBox.Text;
+            MessageBox.Show(string.Format(resManager.GetString("hiName"), name));
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string cultureStr = languageCombobox.GetItemText(languageCombobox.SelectedItem);
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo(cultureStr);
+        }
+    }
+}
